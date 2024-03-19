7d2d5d72c66f453ecd1070408ff998914b463fdf
Kevin <kevxemail@gmail.com>
2024-03-19 02:33:07 -0400

    Implemented additional logic to generate unique and identifiable object names for objects being uploaded to s3

--- bot/app/fervent.py
+++ bot/app/fervent.py
@@ -13,7 +13,6 @@ import requests
 bot = commands.Bot(command_prefix="!", intents = Intents.all()) # Set the permissions that this bot will have, right now only need it to send messages
 sentiment_pipeline = pipeline(model="cardiffnlp/twitter-roberta-base-sentiment-latest") # Utilize roBERTa which was fine tuned on Twitter data
 previous_time = 0 # Keep track of how much time it has been since this bot has last sent an image
-sentiment_names = dict()
 
 # Set up S3 client
 ACCESS_KEY, SECRET_KEY = getKeys()
@@ -24,26 +23,24 @@ s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECR
 async def on_ready():
     print('Logged in as {0.user}'.format(bot)) # Indicate that this bot is ready
 
-def upload_image_to_s3(image_url, sentiment):
+def upload_image_to_s3(guild_id, image_url, sentiment):
     try:
         # Download image from URL
         response = requests.get(image_url)
-        if response.status_code == 200:
-            # Save image to a temporary file to upload to s3
+        if response.status_code == 200: # 200 means ok
+            # Save image to a temporary file to upload to s3, save it to the same directory that fervent.py is in
             with open('app/temp_image.jpg', 'wb') as f:
                 f.write(response.content)
 
             # Upload file to S3 bucket
-            object_key = f"{sentiment}/{os.path.basename(image_url)}"
-            s3.upload_file('app/temp_image.jpg', getBucketName(), object_key)
-            sentiment_names[sentiment] = object_key # Save the sentiment's URL for message use later!
-            print(sentiment_names)
+            object_key = f"{guild_id}/{sentiment}/{sentiment}.jpg" # Generate an object_key to query from s3 later of the form discord_server_id/sentiment/sentiment.jpg
+            s3.upload_file('app/temp_image.jpg', getBucketName(), object_key) # upload_file takes in 1. file path 2. Bucket name 3. Object key to query
             # Generate presigned URL for the uploaded file
 
             file_url = s3.generate_presigned_url('get_object', Params={'Bucket': getBucketName(), 'Key': object_key}) # Get the presigned URL
             return file_url
         else:
-            print("Failed to download image from URL.")
+            print("Failed to download image from URL.") # Indicates failure from downloading the image with the discord image.url link
             return None
     except Exception as e:
         print(f"Error uploading image to S3: {e}") # Something went wrong during upload to s3
@@ -58,15 +55,15 @@ async def setimage(ctx, sentiment: str):
     if sentiment != 'neutral' and sentiment != 'positive' and sentiment != 'negative': # Check if they are trying to upload an image for the incorrect sentiment
         await ctx.send("The only available sentiments in which you can upload images is 'positive', 'negative', and 'neutral. Please set one image for each of these sentiments only.")
         return
-    image_list = ctx.message.attachments
+    image_list = ctx.message.attachments # Grab the image_list from any attachments
+
     if len(image_list) != 1: # Check if they are trying upload more or less than one image
         await ctx.send("You must attach EXACTLY ONE image to set this sentiment's image configuration. No more no less.")
         return
     # Now we can start the image uploading logic
     image = image_list[0]
-    print(image.url)
 
-    file_url = upload_image_to_s3(image.url, sentiment)
+    file_url = upload_image_to_s3(ctx.guild.id, image.url, sentiment) # Send in the .url parameter associated with the image, the ID associated with the discord server, and the sentiment for this user's command
     
     if file_url:
         await ctx.send("Image uploaded successfully!")
@@ -91,13 +88,13 @@ async def on_message(message):
         await message.reply("positive prediction") # Directly reply to the author instead
     elif prediction[0]['label'] == 'negative':
         try:
-            response = s3.get_object(Bucket=getBucketName(), Key=sentiment_names['negative'])
+            response = s3.get_object(Bucket=getBucketName(), Key=f"{message.guild.id}/{'negative'}/{'negative'}.jpg")
             print(response)
             image_data = response['Body'].read()
-            await message.reply("negative prediction", file=discord.File(io.BytesIO(image_data), filename='negative_image.png'))
+            await message.reply(file=discord.File(io.BytesIO(image_data), filename='negative_image.png'))
         except Exception as e: # Error occured with retrieving the image
             print(f"Error retrieving image from S3: {e}")
-            await message.reply("Error retrieving image.")
+            # Don't print anything in this case, typically means an image hasn't been uploaded for this sentiment yet
     else:
         await message.reply("neutral prediction")
     
