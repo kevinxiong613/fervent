e49988a0a20a9682d797465e2944da0c8c8e9065
Kevin <kevxemail@gmail.com>
2024-03-19 01:47:42 -0400

    Implemented AWS S3 upload code logic

--- bot/app/fervent.py
+++ bot/app/fervent.py
@@ -1,17 +1,50 @@
 from discord import Intents
 from discord.ext import commands
-from run import run
+from run import run, getBucketName, getKeys
 from transformers import pipeline
 import time # Keep track of the time
+import boto3
+import os
+import requests
 
 
 bot = commands.Bot(command_prefix="!", intents = Intents.all()) # Set the permissions that this bot will have, right now only need it to send messages
 sentiment_pipeline = pipeline(model="cardiffnlp/twitter-roberta-base-sentiment-latest") # Utilize roBERTa which was fine tuned on Twitter data
 previous_time = 0 # Keep track of how much time it has been since this bot has last sent an image
+names = dict()
+
+# Set up S3 client
+ACCESS_KEY, SECRET_KEY = getKeys()
+s3 = boto3.client('s3', aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+
 
 @bot.event
 async def on_ready():
     print('Logged in as {0.user}'.format(bot)) # Indicate that this bot is ready
+
+def upload_image_to_s3(image_url, sentiment):
+    try:
+        # Download image from URL
+        response = requests.get(image_url)
+        if response.status_code == 200:
+            # Save image to a temporary file to upload to s3
+            with open('app/temp_image.jpg', 'wb') as f:
+                f.write(response.content)
+
+            # Upload file to S3 bucket
+            object_key = f"{sentiment}/{os.path.basename(image_url)}"
+            s3.upload_file('app/temp_image.jpg', getBucketName(), object_key)
+            names[sentiment] = object_key # Save the sentiment's URL for message use later!
+            # Generate presigned URL for the uploaded file
+
+            file_url = s3.generate_presigned_url('get_object', Params={'Bucket': getBucketName(), 'Key': object_key}) # Get the presigned URL
+            return file_url
+        else:
+            print("Failed to download image from URL.")
+            return None
+    except Exception as e:
+        print(f"Error uploading image to S3: {e}") # Something went wrong during upload to s3
+        return None
 
 @bot.command() # Command in the form of "!setimage sentiment" but they also must have an image attached
 async def setimage(ctx, sentiment: str):
@@ -27,9 +60,15 @@ async def setimage(ctx, sentiment: str):
         await ctx.send("You must attach EXACTLY ONE image to set this sentiment's image configuration. No more no less.")
         return
     # Now we can start the image uploading logic
-
     image = image_list[0]
-    await ctx.send("End")
+    print(image.url)
+
+    file_url = upload_image_to_s3(image.url, sentiment)
+    
+    if file_url:
+        await ctx.send("Image uploaded successfully!")
+    else:
+        await ctx.send("Image not uploaded successfully!")
 
 @bot.event
 async def on_message(message):
@@ -49,6 +88,7 @@ async def on_message(message):
     if prediction[0]['label'] == 'positive': # Set conditions to check what the prediction is
         await message.reply("positive prediction") # Directly reply to the author instead
     elif prediction[0]['label'] == 'negative':
+        s3.get_object(Bucket=getBucketName(), Key='your-object-key')
         await message.reply("negative prediction")
     else:
         await message.reply("neutral prediction")
